5d29e88967e093cbc4846fac52e07d619513451e
wooyoung0830 <thddndud0830@gmail.com>
2022-08-10 00:07:14 +0900

    Q11399

---
+++ Q11399/11399.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+let input = fs.readFileSync('Q11399/input.txt').toString().split('\n');
+const peopleNumber = input[0]; // 사람들 총 인원 수 
+let people = input[1].split(' ');
+// console.log(people);
+
+people = people.map((num)=> parseInt(num));
+
+// 오름차순
+// people.sort(function(a,b){
+//     return a-b;
+// });
+
+// 화살표 함수로 간결하게
+people.sort((a,b) => a - b)
+// console.log(people);
+
+let sum = 0;
+for (var i = 1;  peopleNumber >= i; i++) {
+    for(var j = 0; j < i; j++){
+        sum = sum + people[j]
+    }
+}
+console.log(sum);
