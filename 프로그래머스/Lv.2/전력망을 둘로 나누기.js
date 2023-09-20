9bf49d895664d5ae503b5fb0cd1aeb089551f0b5
f0ever0 <thddndud0830@gmail.com>
2023-09-20 15:22:32 +0900

    0920 풀이

--- 프로그래머스/Lv.2/전력망을 둘로 나누기.js
+++ 프로그래머스/Lv.2/전력망을 둘로 나누기.js
@@ -1,3 +1,44 @@
+const solution2 = (n, wires) => {
+  let network = {};
+  let result = n;
+
+  wires.forEach((wire) => {
+    let [start, end] = wire;
+    !network[start] ? (network[start] = [end]) : network[start].push(end);
+    !network[end] ? (network[end] = [start]) : network[end].push(start);
+  });
+
+  function countNode(start, exception) {
+    // start에서 exception을 제외하고 연결되어 있는 노드의 갯수
+    let count = 0;
+    let visited = [];
+    let queue = [start];
+    visited.push(start);
+
+    while (queue.length) {
+      let currentNode = queue.pop();
+
+      network[currentNode].forEach((node) => {
+        if (!visited.includes(node) && node !== exception) {
+          queue.push(node);
+          visited.push(node);
+        }
+      });
+
+      count++;
+    }
+
+    return count;
+  }
+
+  wires.forEach((wire) => {
+    let [a, b] = wire;
+    result = Math.min(result, Math.abs(countNode(a, b) - countNode(b, a)));
+  });
+
+  return result;
+};
+
 const solution = (n, wires) => {
   let links = {};
   let answer = Number.MAX_SAFE_INTEGER;
--- 프로그래머스/Lv.2/카펫.js
+++ 프로그래머스/Lv.2/카펫.js
@@ -1,3 +1,17 @@
+const solution2 = (brown, yellow) => {
+  let total = brown + yellow;
+  // yellow = 6 * 4
+  // total = 8 * 6
+
+  for (let x = 1; x <= brown; x++) {
+    for (let y = 1; y <= x; y++) {
+      if (total === x * y && yellow === (x - 2) * (y - 2)) {
+        return [x, y];
+      }
+    }
+  }
+};
+
 const solution = (brown, yellow) => {
   const total = brown + yellow;
 
